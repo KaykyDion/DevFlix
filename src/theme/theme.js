5fee76c385d83de793600730b68feaf686564da4
Igorrst <igorrian.cntgm18@gmail.com>
2024-08-13 17:32:05 -0300

    Implementando theme

---
+++ src/theme/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    colors: {
+        primary: "#4B0082",
+        secondary: "#6A0DAD",
+        background: "#000000",
+        cardBackground: "#202020",
+        text: "#FFFFFF",
+        footerBackground: "#00008B" 30%,
+        footerText: "#FFFFFF",
+    },
+    fonts: {
+        main: "'roboto', sans serif",
+    },
+};
