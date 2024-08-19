26ef6633cfd3b08dffc0d829f00bae240280655d
Kayky Dion <kayky.dion@gmail.com>
2024-08-18 22:06:23 -0300

    Criando listagem de filmes

--- src/components/MovieItem/styles.js
+++ src/components/MovieItem/styles.js
@@ -1,21 +1,20 @@
 import styled from "styled-components";
 
-export const MovieCard = styled.div`
+export const MovieCard = styled.li`
 	border-radius: 10px;
-	width: 180px;
-	height: 220px;
+	min-width: 180px;
+	min-height: 220px;
 	transition: 0.4s;
 	cursor: pointer;
 
 	&:hover {
-		transform: scale(1.05);
-		box-shadow: 0 0 3px #cc00cc, 0 0 6px #cc00cc, 0 0 9px #cc00cc,
-			0 0 12px #cc00cc;
+		transform: translateY(-4px);
 	}
 
 	img {
-		width: 100%;
-		height: 100%;
 		border-radius: 10px;
+		height: 100%;
+		width: 100%;
+		object-fit: cover;
 	}
 `;
